db25399f5341592b2f8a642a18d38571652daee6
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-10 12:33:14 -0300

    refactor: criadas as rotas para as questoes 1 e 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Container from './App.styles';
 import Home from './Pages/Home/Home';
+import Question1 from './Pages/Question1/Question1';
+import Question2 from './Pages/Question2/Question2';
 
 const App = () => (
   <Container>
     <Routes>
+      <Route path="/questao1" element={<Question1 />} />
+      <Route path="/questao2" element={<Question2 />} />
       <Route path="/" element={<Home />} />
     </Routes>
   </Container>

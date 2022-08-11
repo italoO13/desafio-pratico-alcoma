ca39c2468b2a225145f9a334f155388448293dcc
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-10 22:39:42 -0300

    fix: enviado objeto como parametro para a função userByClient

--- src/Pages/Question2/Question2.jsx
+++ src/Pages/Question2/Question2.jsx
@@ -3,6 +3,7 @@ import Card from '../../Components/Card/Card';
 import Header from '../../Components/Header/Header';
 import generalAcess from '../../Middleware/acessAll';
 import userByClient from '../../Middleware/infosClients';
+import { Container, Cards, Title } from './Question2.styles';
 
 const Question2 = () => {
   const [acessAll, setAcessAll] = useState({
@@ -17,7 +18,12 @@ const Question2 = () => {
       setAcessAll(await generalAcess());
     };
     const fetchClientAcess = async () => {
-      setAcessClient(await userByClient('vagalume', '2022/08/01', '2022/08/10'));
+      const obj = {
+        selectClient: 'vagalume',
+        initialDate: '2022-08-01',
+        finalDate: '2022-08-10',
+      };
+      setAcessClient(await userByClient(obj));
     };
     fetchGeneralAcess();
     fetchClientAcess();
@@ -26,16 +32,16 @@ const Question2 = () => {
   return (
     <>
       <Header />
-      <div>
-        Question2
-        <div id="cards">
+      <Container>
+        <Title>Questão 2</Title>
+        <Cards id="cards">
           <Card title="Pessoas Totais" info={acessAll.general} />
           <Card title="Pessoas Online" info={acessAll.online} />
           <Card title="Pessoas Offline" info={acessAll.offline} />
           <Card title="Pessoas conectadas no período" info={acessClient} />
-        </div>
+        </Cards>
 
-      </div>
+      </Container>
     </>
   );
 };

f6fe9b45e0aab57e6383d93ae3f07e5d3db78fe9
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-10 23:04:17 -0300

    style: estilizado Page de Home

--- src/Pages/Home/Home.styles.js
+++ src/Pages/Home/Home.styles.js
@@ -2,8 +2,22 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: #fff;
-  color: #797a81;
+  color: black;
+  width:100%;
   min-height:100vh;
+  .wrapperHome {
+    margin-top:50px;
+    width:100%;
+    text-align:center;
+  }
+  .wrapperHome img {
+    width:500px;
+  }
+  .wrapperHome h1 {
+    margin-top:20px;
+    font-size:2rem;
+  }
+
 `;
 
 export default Container;

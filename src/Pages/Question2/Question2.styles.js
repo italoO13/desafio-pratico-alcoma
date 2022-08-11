b924cca54fcd31b7bee004dc703ee123ce364d8d
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-10 22:40:06 -0300

    style: estilizado Page question2

---
+++ src/Pages/Question2/Question2.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding:30px;
+  color:black;
+`;
+
+export const Title = styled.div`
+  text-align:center;
+  font-size:2rem;
+  margin-bottom:30px;
+`;
+
+export const Cards = styled.div`
+  width: 60%;
+  min-width:40%;
+  margin:auto;
+`;

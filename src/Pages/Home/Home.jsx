321ecb9d792ca5c4103ac8b6b9d2d506e110dbca
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-08-10 23:04:00 -0300

    feat: criado page de Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Container from './Home.styles';
 import Header from '../../Components/Header/Header';
+import imgHome from '../../imgs/home.png';
 
 const Home = () => (
   <Container>
     <Header />
+    <div className="wrapperHome">
+      <img src={imgHome} alt="Imagem de Home" />
+      <h1>Bem Vindo ao sistema de Clientes da Vagalume !</h1>
+    </div>
   </Container>
 );
 
